274fe96416ec101bcd6da68987cff78c73c9a918
David Lin <lindavid1998@gmail.com>
2023-04-12 08:13:15 -0700

    Add sample data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+import seasonKinImg from './images/season_kin_snowboard_2023.webp';
+import gnuYoungMoneyImg from './images/gnu_young_money_C2E_2023.webp'
+
+const data = [
+	{
+		id: 0,
+		name: 'Season Kin Snowboard 2023',
+		price: 349.3,
+		category: 'snowboard',
+		imgSrc: seasonKinImg,
+	},
+	{
+		id: 1,
+		name: 'GNU Young Money C2E Snowboard - Big Boys 2023',
+		price: 279.99,
+		category: 'snowboard',
+		imgSrc: gnuYoungMoneyImg,
+	},
+	// ...
+];
+
+export default data;
