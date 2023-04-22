e5f31296f244f6237182abcd8cc979a58925d274
David Lin <lindavid1998@gmail.com>
2023-04-21 18:32:05 -0700

    Add styling to Confirmation page

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import '../styles/Confirmation.css'
 
-export default function Confirmation(props) {
+export default function Confirmation() {
   return (
 		<div className='Confirmation'>
-			<div>Your order is confirmed!</div>
-			<div>Thank you</div>
+			<h2>Your order is confirmed!</h2>
+			<h3>Thank you</h3>
 			<Link to='/shop'>
-				<button className='btn-filled'>Shop More</button>
+				<button className='btn-translate-on-hover btn-filled btn-medium'>
+					Shop More
+				</button>
 			</Link>
 		</div>
 	);
